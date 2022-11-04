e7a538604e24425f1775de2c8a9b5a64c165f161
Matheus de Deus <matheus_deusr@hotmail.com>
2022-11-03 21:54:36 -0300

    Componentizando os cards com js

---
+++ main.js
@@ -0,0 +1,31 @@
+function createGame(player1, hours, player2){
+    return `   
+        <li>
+            <img src="./assets/bandeira-${player1}.svg" alt="bandeira do ${player1}">
+            <strong>${hours}</strong>
+            <img src="./assets/bandeira-${player2}.svg" alt="bandeira do ${player2}">
+        </li>
+    `
+}
+
+function creatCard(date, day, games){
+    return `
+        <div class="card">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+            ${games}
+            </ul>
+        </div>
+    `
+}
+
+document.querySelector("#app").innerHTML = `
+    <header>
+        <img src="./assets/logo.svg" alt="logo da NLW">
+    </header>
+
+    <main id="cards">
+        ${creatCard("24/11", "Quinta", createGame('brasil', '16:00', 'camaroes'))}
+        ${creatCard("24/11", "Quinta", createGame('brasil', '08:00', 'camaroes') + createGame('brasil', '13:00', 'camaroes'))}
+    </main>
+`;
